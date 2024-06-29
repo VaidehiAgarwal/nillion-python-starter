113321290a7ddc2f62e6e357189b9adbc69109f5
VaidehiAgarwal <kvaidehiagarwal@gmail.com>
2024-06-29 08:03:05 +0000

    my new nada program

--- programs/my_first_program.py
+++ programs/my_first_program.py
@@ -1,13 +1,24 @@
 from nada_dsl import *
 
-def nada_main():
-    party1 = Party(name="Party1")
-    my_int1 = SecretInteger(Input(name="my_int1", party=party1))
-    my_int2 = SecretInteger(Input(name="my_int2", party=party1))
-
-    # write the computation for your program here - use my_int1 and my_int2 as inputs
-    # Example computation: adding my_int1 and my_int2
-    result = my_int1 + my_int2
-
-    # return the result of the computation as output
-    return [Output(result, "my_output", party=party1)]
+def secure_auction_system():
+    # Define parties
+    auctioneer = Party(name="Auctioneer")
+    bidder1 = Party(name="Bidder1")
+    bidder2 = Party(name="Bidder2")
+    
+    # Input bids from each bidder (using SecretInteger)
+    bid1 = SecretInteger(Input(name="bid1", party=bidder1))
+    bid2 = SecretInteger(Input(name="bid2", party=bidder2))
+    
+    # Compute the maximum bid
+    max_bid = bid1.max(bid2)
+    
+    # Determine the winning bidder
+    winner = If(max_bid == bid1, "Bidder1",
+                Else("Bidder2"))
+    
+    # Return results as outputs
+    return [
+        Output(max_bid, "max_bid", party=auctioneer),
+        Output(winner, "winner", party=auctioneer)
+    ]
